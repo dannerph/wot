54cc994ebcf592c795add19ee5f648f736d6e0f4
KDeDeDe <juergenrottmeier78@gmail.com>
2016-01-07 16:34:29 +0100

    blubb

--- coap_heater.lua
+++ coap_heater.lua
@@ -12,3 +12,20 @@ function heaterON()
 function heaterOFF()
     gpio.write(0,gpio.LOW)
     end
+
+
+function myCondition(cond)
+    status = gpio.read(0)
+end
+
+
+function ledIsOn()
+    var = false
+        if gpio.read(0) == 0 then
+         var = false
+        else  
+           var = true 
+           end
+        return var
+        
+end
--- config.lua
+++ config.lua
@@ -1,3 +1,4 @@
 --config.lua
 
 thingType = "alarm"
+thingType = "heater"
