5b303a1f038e5d677d4cfd5b31583087064f5858
Florian Bieringer <florian.bieringer@uni-passau.de>
2016-01-12 15:14:31 +0100

    temp coap

--- coap_temp.lua
+++ coap_temp.lua
@@ -1,7 +1,7 @@
 --coap_temp.lua
 cs:var(coap.GET, "temp", 0, 1)
 tmr.alarm(0, 1000, 1, function()
-    currentTemp = getTemp(3)
+    currentTemp = "{\"value\":\""..getTemp(3).."\"}"
     if (temp ~= currentTemp) then
         temp = currentTemp
         cs:changed('temp')
